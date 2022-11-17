ba0edf4ecbcd07a9517f0ac03f87e07941e7ee27
Alicia <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2022-11-17 16:31:21 +0700

    initial commit

---
+++ E1.py
@@ -0,0 +1,27 @@
+scoreList = []
+
+numbers = int(input('How many score do you want to input : '))
+
+for i in range(numbers):
+    score = int(input("score" + str(i) + ":"))
+    scoreList.append(score)
+    
+maxValue = scoreList[0]
+minValue = scoreList[0]
+sum = 0
+
+for score in scoreList:
+    if maxValue < score:
+        maxValue = score
+        
+for score in scoreList:
+    if minValue > score:
+        minValue = score
+        
+for score in scoreList:
+    sum = sum + score
+averageValue = sum/numbers
+
+print(maxValue)
+print(minValue)
+print(averageValue)
